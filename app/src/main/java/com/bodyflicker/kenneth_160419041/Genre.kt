f84b11a3d1e171f7a4fa52d595e7fb3181989d72
Kenneth Manuel <kennethlieyanto99@gmail.com>
2021-06-26 16:52:26 +0700

    .

--- app/src/main/java/com/bodyflicker/kenneth_160419041/Genre.kt
+++
@@ -1,5 +0,0 @@
-package com.bodyflicker.kenneth_160419041
-
-data class Genre(val id:Int, val nama:String) {
-    override fun toString() = nama
-}
--- app/src/main/java/com/bodyflicker/kenneth_160419041/Movie.kt
+++
@@ -1,5 +0,0 @@
-package com.bodyflicker.kenneth_160419041
-
-data class Movie(val title: String, val rating: String, val showDate: String, val url: String, val imdbUrl: String) {
-
-}
